336f87884c24451778cb7db930f1c548f26dc56f
tyfkda <tyfkda@gmail.com>
2023-05-11 22:43:20 +0900

    Xを最初に置く
    Before:
    Total: Solution=2339, check=25839560
    cargo run --release -- --size=6  4.10s user 0.05s system 80% cpu 5.166 total
    
    After:
    Total: Solution=2339, check=3255401
    cargo run --release -- --size=6  0.64s user 0.04s system 38% cpu 1.760 total

--- rust/src/pentomino/solver/naive_solver.rs
+++ rust/src/pentomino/solver/naive_solver.rs
@@ -34,7 +34,30 @@ impl NaiveSolver {
     pub fn solve(&mut self) {
         self.check_count = 0;
         self.arranges.fill(None);
-        self.solve_recur(0, 0);
+
+        // Check X piece first.
+        if let Some(ip) = self.pieces.iter().position(|piece| piece.shapes.len() == 1) {
+            self.solve_x(ip);
+        } else {
+            self.solve_recur(0, 0);
+        }
+    }
+
+    fn solve_x(&mut self, ip: usize) {
+        // Put X piece first in top left quad.
+        let is = 0;
+        for x in 0..(self.w + 1) / 2 {
+            for y in 0..(self.h + 1) / 2 {
+                if x == 0 && y == 0 { continue; }
+                self.check_count += 1;
+                if let Some(ofsy) = can_put_shape(self.bitboard, self.w, self.h, &self.pieces[ip].shapes[is], x, y) {
+                    self.bitboard = put_shape(self.bitboard, self.w, self.h, &self.pieces[ip].shapes[is], x, y - ofsy, true);
+                    self.arranges[ip] = Some(PieceArrange {x, y: y - ofsy, shape: 0});
+                    self.solve_recur(0, 0);
+                    self.bitboard = put_shape(self.bitboard, self.w, self.h, &self.pieces[ip].shapes[is], x, y - ofsy, false);
+                }
+            }
+        }
     }
 
     fn solve_recur(&mut self, x: usize, y: usize) {
