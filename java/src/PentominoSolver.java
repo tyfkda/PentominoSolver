3620d9b6b7c4ec0e4bb1d1e4a0f48ee43d63bd41
tyfkda <tyfkda@gmail.com>
2024-12-20 19:21:52 +0900

    図として表示

--- java/src/PentominoSolver.java
+++ java/src/PentominoSolver.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
 import java.util.HashSet;
 
 import pentomino.BoardConfig;
 import pentomino.PieceArrange;
 import pentomino.Solver;
+import pentomino.BitBoard;
 import pentomino.component.Piece;
+import pentomino.component.Shape;
 
 public class PentominoSolver {
     public static void main(String[] args_) throws Exception {
@@ -16,7 +19,11 @@ public class PentominoSolver {
         Piece[] pieces = Piece.createShapes(boardW, boardH);
 
         Solver solver = new Solver(args.boardConfig, pieces);
-        Solver.Callback callback = new PrintResultColor(pieces, args.boardConfig);
+        Solver.Callback callback;
+        if (args.figure)
+            callback = new PrintResultFigure(pieces, args.boardConfig);
+        else
+            callback = new PrintResultColor(pieces, args.boardConfig);
 
         solver.setCallback(callback);
         solver.solve();
@@ -26,6 +33,7 @@ public class PentominoSolver {
 
     private static Args parseArgs(String[] args) {
         BoardConfig boardConfig = BoardConfig._6x10;
+        boolean figure = false;
         int i;
         for (i = 0; i < args.length; ++i) {
             if ((args[i].equals("-s") || args[i].equals("--size")) && i + 1 < args.length) {
@@ -42,6 +50,10 @@ public class PentominoSolver {
                 }
                 continue;
             }
+            if (args[i].equals("-f") || args[i].equals("--figure")) {
+                figure = true;
+                continue;
+            }
 
             break;
         }
@@ -55,7 +67,7 @@ public class PentominoSolver {
             return null;
         }
 
-        return new Args(boardConfig);
+        return new Args(boardConfig, figure);
     }
 }
 
@@ -111,10 +123,111 @@ class PrintResultColor implements Solver.Callback {
     }
 }
 
+class PrintResultFigure implements Solver.Callback {
+    private static final char[] GRID_CHARS = new char[] {
+        ' ', ' ', ' ', '━',
+        ' ', '┏', '┓', '┳',
+        ' ', '┗', '┛', '┻',
+        '┃', '┣', '┫', '╋',
+    };
+
+    private final Piece[] pieces;
+    private final BoardConfig boardConfig;
+
+    public PrintResultFigure(Piece[] pieces, BoardConfig boardConfig) {
+        this.pieces = pieces;
+        this.boardConfig = boardConfig;
+    }
+
+    public void found(PieceArrange[] arranges) {
+        int w = boardConfig.width;
+        int h = boardConfig.height;
+        int w2 = w + 1;
+        int h2 = h * 2 + 1;
+
+        char[] placed = new char[w2 * h2];
+        Arrays.fill(placed, '.');
+        for (int i = 0; i < pieces.length; ++i) {
+            Piece piece = pieces[i];
+            PieceArrange arrange = arranges[i];
+            Shape shape = piece.shapes[arrange.shape];
+            long bitpat = shape.bitpat;
+            while (bitpat != 0) {
+                int ofs = BitBoard.trailingZeros(bitpat);
+                int xofs = ofs % w;
+                int yofs = ofs / w;
+                placed[((arrange.y + yofs) * 2 + 1) * w2 + (arrange.x + xofs + 1)] = piece.name;
+                bitpat &= bitpat - 1;  // Remove lowest bit.
+            }
+        }
+
+        boolean hlines[][] = new boolean[h][w + 1];
+        for (int y = 0; y < h; ++y) {
+            for (int x = 0; x < w + 1; ++x) {
+                char c1 = x > 0 ? placed[(y * 2 + 1) * w2 + x] : ' ';
+                char c2 = x < w ? placed[(y * 2 + 1) * w2 + (x + 1)] : ' ';
+                hlines[y][x] = c1 != c2;
+            }
+        }
+
+        boolean vlines[][] = new boolean[h + 1][w];
+        for (int x = 0; x < w; ++x) {
+            for (int y = 0; y < h + 1; ++y) {
+                char c1 = y > 0 ? placed[(y * 2 - 1) * w2 + x + 1] : ' ';
+                char c2 = y < h ? placed[(y * 2 + 1) * w2 + x + 1] : ' ';
+                vlines[y][x] = c1 != c2;
+            }
+        }
+
+        // Put grid lines.
+        for (int x = 0; x < w + 1; ++x) {
+            for (int y = 0; y < h + 1; ++y) {
+                int i = 0;
+                if (y < h && hlines[y    ][x])  i |= 1;
+                if (y > 0 && hlines[y - 1][x])  i |= 2;
+                if (x < w && vlines[y][x    ])  i |= 4;
+                if (x > 0 && vlines[y][x - 1])  i |= 8;
+                placed[(y * 2) * w2 + x] = GRID_CHARS[i];
+            }
+        }
+        for (int x = 0; x < w + 1; ++x) {
+            for (int y = 0; y < h; ++y) {
+                if (hlines[y][x]) {
+                    placed[(y * 2 + 1) * w2 + x] = GRID_CHARS[3];
+                }
+            }
+        }
+
+        // Transpose board to reduce lines.
+        boolean[] letter_appeared = new boolean[26];
+        Arrays.fill(letter_appeared, false);
+        for (int x = 0; x < w2; ++x) {
+            StringBuilder sb = new StringBuilder();
+            for (int y = 0; y < h2; ++y) {
+                int i = y * w2 + x;
+                char c = placed[i];
+                if ('A' <= c && c <= 'Z') {
+                    int l = (int)c - (int)'A';
+                    if (!letter_appeared[l]) {
+                        letter_appeared[l] = true;
+                    } else {
+                        c = ' ';
+                    }
+                }
+                sb.append(c);
+            }
+            System.out.println(sb.toString());
+        }
+        System.out.println();
+    }
+}
+
 class Args {
     final BoardConfig boardConfig;
+    final boolean figure;
 
-    public Args(BoardConfig boardConfig) {
+    public Args(BoardConfig boardConfig, boolean figure) {
         this.boardConfig = boardConfig;
+        this.figure = figure;
     }
 }
